b4bef9c8eb91c48f607ded3cbd972a4bcf000d99
Oner Berk <oner_berk@hotmail.com>
2020-06-17 19:33:26 +0200

    inscrpiton ok form

--- PyBlog.py
+++ PyBlog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+
+app.config['SECRET_KEY']='674bad2b681ff4b8d50dc40c7f158b7d'
 posts =[
     {
         'author':'Oner berk',
@@ -27,6 +30,18 @@ def hello():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/registration", methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Le compte de {form.username.data} est crée ! ', 'success')
+        # return redirect(url_for(''))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run()
